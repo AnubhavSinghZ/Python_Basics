7ca8f14fe7dc9b50af9753921764fc458a3def5b
AnubhavSinghZ <aanusingh927@gmail.com>
2026-06-11 01:28:06 +0530

    Add code of alien

--- alien_color.py
+++ alien_color.py
@@ -1,4 +1,15 @@
 alien_color=input("Choose the color(RED, GREEN, YELLOW)")
-if (alien_color==green) or (alien_color==GREEN):
+if (alien_color=="green") or (alien_color=="GREEN"):
     print("YOU HAVE EARNED 5 POINTS")
-  
+else:
+    print()
+if (alien_color=="green") or (alien_color=="GREEN"):
+    print("YOU HAVE EARNED 5 POINTS")
+else:
+    print("YOU HAVE EARNED 10 POINTS")
+if (alien_color=="green") or (alien_color=="GREEN"):
+    print("YOU HAVE EARNED 5 POINTS")
+elif(alien_color=="yellow") or (alien_color=="YELLOW"):
+    print("YOU HAVE EARNED 15 POINTS")
+elif(alien_color=='red') or (alien_color=="RED"):
+    print("YOU HAVE EARNED 20 POINTS")
