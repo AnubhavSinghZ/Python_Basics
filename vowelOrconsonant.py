3c6eae261784a9e2101d90ad28bb25380b3bfa2e
Anubhav Kumar Singh <aanusingh927@gmail.com>
2026-07-07 22:08:09 +0530

    Fix spelling errors and add success message
    Corrected the input prompt spelling and added a success message after checking the character type.

--- vowelOrconsonant.py
+++ vowelOrconsonant.py
@@ -1,4 +1,4 @@
-ch= input("Enetr The Character:::")
+ch= input("Enetr The Character:")
 if(ch=='A' or ch=='E' or ch=='I' or ch=='O' or ch=='U'):
     print("Character is Vowel")
 elif(ch=='a' or ch=='e' or ch=='i' or ch=='o' or ch=='u'):
@@ -7,3 +7,4 @@ elif(ch=='B' or ch=='C' or ch=='D' or ch=='F' or ch=='G' or ch=='H' or ch=='J' o
     print("Charatcter is Consonant")
 elif(ch=='b' or ch=='c' or ch=='d' or ch=='f' or ch=='g' or ch=='h' or ch=='j' or ch=='k' or ch=='l' or ch=='m' or ch=='n' or ch=='p' or ch=='q' or ch=='r' or ch=='s' or ch=='t' or ch=='v' or ch=='w' or ch=='x' or ch=='y' or ch=='z'):
     print("Character is Consonant")
+print("You have successfully cheched")
