723973c7e5f07e6b0089c1006b8de2b76c26fc35
AnubhavSinghZ <aanusingh927@gmail.com>
2026-07-10 21:30:35 +0530

    track file

---
+++ track_files.py
@@ -0,0 +1,11 @@
+print("Enter File Name")
+file=[]
+for files in file:
+    if file in file:
+      end=".txt"
+    elif file in file:
+      end='.doc'
+    else:
+      end='error'
+print(file)
+print("You have successfully traked")
