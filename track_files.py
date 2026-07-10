a680171a32fd677ec99a2e83af09a9df4c538489
AnubhavSinghZ <aanusingh927@gmail.com>
2026-07-10 21:33:29 +0530

    Add a code

--- track_files.py
+++ track_files.py
@@ -9,3 +9,8 @@ for files in file:
       end='error'
 print(file)
 print("You have successfully traked")
+
+# NOTE
+# This code run nothing 
+# It is only for practice 
+# this is only syntax checking code for python that where it give ERROR  or HOW CAN WE RUN IT?
