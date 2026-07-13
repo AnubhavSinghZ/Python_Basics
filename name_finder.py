5d49c61b5c97310b7c468961ae764113f7b0461d
AnubhavSinghZ <aanusingh927@gmail.com>
2026-07-13 13:43:20 +0530

    Change a variable

--- name_finder.py
+++ name_finder.py
@@ -1,8 +1,8 @@
 import csv
-search_name = input("Enter name to search: ")
+find_name = input("Enter name to search: ")
 
 with open("data.csv", mode="r") as file:
     reader = csv.reader(file)
     for row in reader:
-        if row[0].lower() == search_name.lower():
+        if row[0].lower() == find_name.lower():
             print("Found match:", row)
