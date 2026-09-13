3a355ba26561c16d590e0827987165a0d08685dc
AnubhavSinghZ <aanusingh927@gmail.com>
2026-09-13 20:33:50 +0530

    new delete method in oops file

--- Class/students.py
+++ Class/students.py
@@ -7,6 +7,8 @@ s1= Student("Karan", 99)
 s2= Student("Arjun", 100)
 print(f"{s1.name} and his marks is {s1.marks}")
 print(f"{s2.name} and his marks is {s2.marks}")
+del s1.name  # this will delete the s1 name 
+print(s1)  
 
 #print(s1.name,s1.marks) Karan,99
 #print(s2.name,s2.marks)  Arjun,100
