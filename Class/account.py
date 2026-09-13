365bd04fd39a363d9527834d8e10a97681df8ec5
AnubhavSinghZ <aanusingh927@gmail.com>
2026-09-13 20:52:34 +0530

    new method od self

--- Class/account.py
+++ Class/account.py
@@ -2,7 +2,7 @@ class Account:
     def __init__(self, acc_no, acc_pas):
         self.acc_no= acc_no
         self.__acc_pas=acc_pas
-    def reset_pass():
+    def reset_pass(self):
         print(self.__acc_pas)
 
 acc1= Account("12345","abcde")
