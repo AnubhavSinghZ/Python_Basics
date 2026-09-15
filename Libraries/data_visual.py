6e7b41e38b40f2b075a211c75b06f4918e37ca53
Anubhav Kumar Singh <aanusingh927@gmail.com>
2026-09-15 22:04:09 +0530

    code with comments

--- Libraries/data_visual.py
+++ Libraries/data_visual.py
@@ -1,11 +1,27 @@
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt  # Import matplotlib for plotting graphs
 
+# Define the x-axis values
 x = [1, 2, 3, 4, 5]
+
+# Define the y-axis values
 y = [2, 4, 6, 8, 10]
 
+# Plot a line graph using x and y values
+# label gives the name of the line
+# color sets the line color to blue
 plt.plot(x, y, label="Growth", color="blue")
+
+# Set the label for the x-axis
 plt.xlabel("Time")
+
+# Set the label for the y-axis
 plt.ylabel("Value")
+
+# Set the title of the graph
 plt.title("Simple Line Plot")
+
+# Display the legend on the graph
 plt.legend()
+
+# Display the graph
 plt.show()
