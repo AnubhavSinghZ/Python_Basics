9cace3fbb7e1635c02c758a1abd74b18bda64822
AnubhavSinghZ <aanusingh927@gmail.com>
2026-09-16 22:55:33 +0530

    new inheritence

---
+++ Class/car.py
@@ -0,0 +1,15 @@
+class Car:
+    @staticmethod
+    def start():
+        print("car started")
+
+    @staticmethod
+    def stop():
+        print("car stopped")
+class ToyotaCar(Car):
+    def __init__(self, name):
+        self.name=name
+car1=ToyotaCar("Fortuner")
+car2=ToyotaCar("Hyryder")
+
+print(car1.start())
