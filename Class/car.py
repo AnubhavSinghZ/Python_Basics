53959ad2b590e184f734b68db2f1562d324bf139
AnubhavSinghZ <aanusingh927@gmail.com>
2026-09-16 23:02:24 +0530

    new line above static method

--- Class/car.py
+++ Class/car.py
@@ -1,4 +1,5 @@
 class Car:
+    color="white"
     @staticmethod
     def start():
         print("car started")
@@ -14,3 +15,4 @@ car2=ToyotaCar("Hyryder")
 
 print(car1.start())
 print(car2.stop())
+print(car1.color)
